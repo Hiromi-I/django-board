8ac704f9944a642627f748bb5d0f1508c9c32ea9
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-09-16 22:47:24 +0900

    Userモデルを作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class Meta(AbstractUser.Meta):
+        db_table = 'custom_user'
+
+    def __str__(self):
+        return self.username
