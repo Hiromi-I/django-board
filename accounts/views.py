7c4515ae9d139bfed485d6796b7792abb02e1e98
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-09-17 22:38:09 +0900

    ユーザー登録機能を実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
-from django.contrib.auth import views
+from django.contrib.auth import views, login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import SignUpForm
 
 
 class LoginView(views.LoginView):
@@ -7,3 +11,14 @@ class LoginView(views.LoginView):
 
 class LogoutView(views.LogoutView):
     pass
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('boards:index')
+    template_name = "accounts/signup.html"
+
+    def form_valid(self, form):
+        valid = super().form_valid(form)
+        login(self.request, self.object)
+        return valid
