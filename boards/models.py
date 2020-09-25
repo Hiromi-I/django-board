9e631b04e493fee1dcb50595302c66733045334b
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-09-25 22:14:53 +0900

    Commentモデルを作成

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -11,3 +12,15 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    class Meta:
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+
+    user = models.ManyToManyField(get_user_model(), related_name="comment")
+    body = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.user}: {self.body}"
